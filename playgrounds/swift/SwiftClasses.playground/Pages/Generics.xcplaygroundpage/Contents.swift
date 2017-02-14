419c7f901663a03f188f1f678d6d7da79a6a3d77
xivol <loshkarev.i@gmail.com>
2017-02-14 12:30:04 +0300

    swift classes errors, protocols, generics

--- playgrounds/swift/SwiftClasses.playground/Pages/Generics.xcplaygroundpage/Contents.swift
+++ playgrounds/swift/SwiftClasses.playground/Pages/Generics.xcplaygroundpage/Contents.swift
@@ -1,6 +1,80 @@
-//: ## Protocols
+//: ## Generics
 //: [Table of Contents](TableOfContents) · [Previous](@previous) · [Next](@next)
 //: ****
+//: ### Generic Functions
+func generic_swap<T> (_ a:inout T, _ b:inout T) {
+    (a,b) = (b,a)
+}
+var i = 32, j = 24
 
+generic_swap(&i, &j)
+(i,j)
+//: ### Protocol Comformance
+func generic_max<T: Comparable>(_ a: T, _ b: T) -> T {
+    return a > b ? a : b
+}
+
+generic_max(i, j)
+//: ### Generic Classes
+struct Stack<Element> {
+    var items = [Element]()
+    mutating func push(_ value: Element) {
+        items.append(value)
+    }
+    mutating func pop() -> Element? {
+        if let value = items.last {
+            items.removeLast()
+            return value
+        }
+        return nil
+    }
+}
+
+var s = Stack<Int>()
+s.push(3)
+s.push(5)
+s.push(8)
+//: ### Extension of Generic Class
+extension Stack: CustomStringConvertible {
+    var description: String {
+        return "\(Element.self):\(items)"
+    }
+}
+s
+//: ### Protocol Conformance
+struct ExclusiveStack<Element> where Element: Hashable {
+    var items = [Element]()
+    mutating func push(_ value: Element) -> Bool {
+        let uniq = Set<Element>(items)
+        if uniq.contains(value) {
+            return false
+        } else {
+            items.append(value)
+            return true
+        }
+    }
+    mutating func pop() -> Element? {
+        if let value = items.last {
+            items.removeLast()
+            return value
+        }
+        return nil
+    }
+}
+
+extension ExclusiveStack: CustomStringConvertible {
+    var description: String {
+        return "\(items)"
+    }
+}
+
+var es = ExclusiveStack<Int>()
+es.push(3)
+es.push(5)
+es.push(8)
+es.push(3)
+es.push(5)
+es.push(8)
+es
 //: ****
 //: [Table of Contents](TableOfContents) · [Previous](@previous) · [Next](@next)
--- playgrounds/swift/SwiftClasses.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
+++ playgrounds/swift/SwiftClasses.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -1,6 +1,84 @@
 //: ## Protocols
 //: [Table of Contents](TableOfContents) · [Previous](@previous) · [Next](@next)
 //: ****
+//: ### Protocol Requrements
+protocol Printable {
+    func printMe()
+}
+//: ### Conforming to a Protocol
+struct Student: Printable {
+    let name: String
+    var course: UInt8
+    let group: UInt8
+    
+    func printMe() {
+        print("\(name) \(course).\(group)")
+    }
+}
+let john = Student(name: "John Smith", course: 1, group: 9)
+//: ### Protocol Adoption
+protocol Person {
+    var name: String { get }
+}
+extension Student: Person {} // already has a name
+//: ### Protocol Inheritace
+import Foundation
+protocol RealPerson: Person {
+    var birthDate: Date { get }
+}
+class Citizen: RealPerson {
+    let name: String
+    let birthDate: Date
+    let country: String
+    
+    init(name: String, birthDate: Date, country: String){
+        self.name = name
+        self.birthDate = birthDate
+        self.country = country
+    }
+}
 
+let formatter = DateFormatter()  // Date ⇆ String
+formatter.dateFormat = "dd.MM.yyyy"
+
+let anna = Citizen(name: "Anna Smith", birthDate: formatter.date(from: "01.01.1997")!, country: "Sweden")
+//: ### Protocol as a Type
+var people = [Person]()
+people.append(john)
+people.append(anna)
+//: ### Checking for Conformance
+for person in people {
+    
+    if person is Printable {
+        print("\(person.name) is printable:", separator: "", terminator: " ")
+        (person as! Printable).printMe()
+    }
+    
+    if let realPerson = person as? RealPerson {
+        print("\(realPerson.name) \(formatter.string(from: realPerson.birthDate))")
+    } else {
+        print("\(person.name) is not a RealPerson")
+    }
+}
+//: ### Associated Types
+protocol Stack {
+    associatedtype Element
+    mutating func push(value: Element)
+    mutating func pop() -> Element?
+}
+
+struct StackOfInt: Stack {
+    var items = [Int]()
+    mutating func push(value: Int) {
+        items.append(value)
+    }
+    mutating func pop() -> Int? {
+        if let value = items.last {
+            items.removeLast()
+            return value
+        }
+        return nil
+    }
+}
 //: ****
 //: [Table of Contents](TableOfContents) · [Previous](@previous) · [Next](@next)
