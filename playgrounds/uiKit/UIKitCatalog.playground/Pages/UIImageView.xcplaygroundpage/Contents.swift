fdf61ab7320340c865168600ea914036035e4ff0
xivol <loshkarev.i@gmail.com>
2017-02-26 09:54:52 +0300

    UIKIT Playground

---
+++ playgrounds/uiKit/UIKitCatalog.playground/Pages/UIImageView.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+import PlaygroundSupport
+
+
+let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 400, height: 250))
+imageView.image = #imageLiteral(resourceName: "swift.png")
+imageView.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+imageView.contentMode = .scaleAspectFit
+
+let swift = [#imageLiteral(resourceName: "be69f2c45a863187c8bc03c8711.jpg"),#imageLiteral(resourceName: "bb95380c1ee8bfbedcbe38b89c4.jpg"),#imageLiteral(resourceName: "beb3f1603f08ed48f29d0654c1a.jpg")]
+imageView.animationImages = swift
+imageView.animationRepeatCount = -1
+imageView.animationDuration = 1.5
+//imageView.startAnimating()
+PlaygroundPage.current.liveView = imageView
